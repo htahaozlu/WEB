3ea08fe73a82f16cf844e260987b73c781d76d20
borayincesoy <borayincesoy@gmail.com>
2024-12-21 19:38:24 +0300

    B

---
+++ WEB/Controllers/LocationsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WEB.Models;
+
+namespace WEB.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private AirlineReservationContext db = new AirlineReservationContext();
+
+        // GET: Locations
+        public ActionResult Index()
+        {
+            var locations = db.Locations.Include(l => l.Parent);
+            return View(locations.ToList());
+        }
+
+        // GET: Locations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return HttpNotFound();
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Create
+        public ActionResult Create()
+        {
+            ViewBag.ParentID = new SelectList(db.Locations, "ID", "Name");
+            return View();
+        }
+
+        // POST: Locations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,CreatedAt,UpdatedAt,DeletedAt,Name,ParentID")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Locations.Add(location);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ParentID = new SelectList(db.Locations, "ID", "Name", location.ParentID);
+            return View(location);
+        }
+
+        // GET: Locations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ParentID = new SelectList(db.Locations, "ID", "Name", location.ParentID);
+            return View(location);
+        }
+
+        // POST: Locations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,CreatedAt,UpdatedAt,DeletedAt,Name,ParentID")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(location).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ParentID = new SelectList(db.Locations, "ID", "Name", location.ParentID);
+            return View(location);
+        }
+
+        // GET: Locations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Location location = db.Locations.Find(id);
+            if (location == null)
+            {
+                return HttpNotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Location location = db.Locations.Find(id);
+            db.Locations.Remove(location);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
